19f425fb80619480a1e533ca00e6e46429ae96ce
Andrei <dandrei.web@gmail.com>
2023-09-07 17:29:53 +0200

    installed sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Button from "react-bootstrap/Button";
+import "./assets/sass/style.scss";
 
 function App() {
     return (
         <>
-            <Button>Test</Button>
+            <Button className="hello">Test</Button>
         </>
     );
 }
